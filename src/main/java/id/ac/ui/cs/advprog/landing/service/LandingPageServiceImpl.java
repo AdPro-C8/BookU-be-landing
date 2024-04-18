9aa10c64ae3d7ca215ee91be1f7865b95d8e31f5
facesofgoblin <buatkuliahrana@gmail.com>
2024-04-19 00:27:23 +0700

    Implement method factory di service

--- src/main/java/id/ac/ui/cs/advprog/landing/service/LandingPageServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/landing/service/LandingPageServiceImpl.java
@@ -13,9 +13,9 @@ public class LandingPageServiceImpl implements LandingPageService {
     public List<Book> getBestSellingBooks() {
         // Sementara pakai data dummy
         return Arrays.asList(
-                new Book(1L, "Book One", "/images/book1.jpg", 150),
-                new Book(2L, "Book Two", "/images/book2.jpg", 120),
-                new Book(3L, "Book Three", "/images/book3.jpg", 100)
+                Book.createBestSellerBook(1L, "Book One", "/images/book1.jpg", 150),
+                Book.createBestSellerBook(2L, "Book Two", "/images/book2.jpg", 120),
+                Book.createBestSellerBook(3L, "Book Three", "/images/book3.jpg", 100)
         );
     }
 }
